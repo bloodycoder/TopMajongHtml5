fa9ec2e0146c310d0480db05a444e08ce6885ed5
Picard Xie <picardxie@foxmail.com>
2017-07-04 15:00:25 +0800

    change pic.php
    修复头像太大bug

--- ZhenZhou/Four/replayData/pic.php
+++ ZhenZhou/Four/replayData/pic.php
@@ -9,5 +9,16 @@ curl_setopt($curl,CURLOPT_TIMEOUT,60);
 $data = curl_exec($curl); 
 curl_close($curl);
 echo $data;
-file_put_contents($_GET["usrid"].".jpg", $data);
+$file_name=$_GET["usrid"].".jpg";
+file_put_contents($file_name, $data);
+/* use GD process picture*/
+echo $_GET["picurl"];
+list($width, $height) = getimagesize($file_name);
+$newwidth = 640;
+$newheight = 640;
+$im_raw = imagecreatefromjpeg($file_name);
+$im_new = imagecreatetruecolor($newwidth, $newheight);
+imagecopyresized($im_new, $im_raw, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+imagejpeg($im_new,$file_name);
+var_dump(gd_info());
 ?>
